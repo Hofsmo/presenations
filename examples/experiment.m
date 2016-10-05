e883ff360a641a6ca02fdf523bb355efb4b0e7ce
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2016-10-05 15:19:30 +0200

    On branch vc  Changes to be committed: 	modified:   examples/experiment.m 	new file:   examples/experiment.m~ 	new file:   pictures/Git-Icon.eps 	modified:   presentation.pdf 	modified:   presentation.tex 	new file:   tex/git.tex 	modified:   tex/what.tex

---
+++ examples/experiment.m
@@ -0,0 +1,11 @@
+%% Don't do this at home kids
+
+% Read data set 1
+data=read_data('data1.csv');
+df = process_data(data, 0.02, 1);
+plot(df);
+
+% Read data set 1
+data=read_data('data2.csv');
+df = process_data(data, 0.02, 1);
+plot(df);
